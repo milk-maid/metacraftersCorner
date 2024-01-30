48d37b07396049f96dd651e87a00cc8fb7eee0f3
milk-maid <roqbell@gmail.com>
2024-01-30 02:45:55 +0100

    the backend  smart contract

--- SCM-Starter/contracts/Assessment.sol
+++ SCM-Starter/contracts/Assessment.sol
@@ -3,58 +3,38 @@ pragma solidity ^0.8.9;
 
 //import "hardhat/console.sol";
 
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+contract FrontCraft {
+    string public name;
+    uint256 public remains;
+    address payable public defaultAdd;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    function Deposit(uint256 _amount, string calldata _name) public payable returns(uint256){
+        uint _previousremains = remains;
+        name = _name;
+        remains = remains + _amount;
+        assert(remains == _previousremains + _amount); //to ensure tx success
+        emit stakeSome(_amount);
+        return remains;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
+    function Withdrawal(uint256 _comotKudiAmount, string calldata _name) public returns(uint256){ 
+        uint _previousremains = remains;
+        if (remains < _comotKudiAmount) {
+            revert Insufficientremains({
+                remains: remains,
+                comotKudiAmount: _comotKudiAmount
             });
         }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        remains -= _comotKudiAmount;
+        name  = name;
+        assert(remains == (_previousremains - _comotKudiAmount));
+        emit comotKudi(_comotKudiAmount);
+        return remains;
     }
+
+    event stakeSome(uint256 indexed amount);
+    event comotKudi(uint256 indexed amount);
+    error Insufficientremains(uint256 remains, uint256 comotKudiAmount);
+
 }
+    
